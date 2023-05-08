7fccc74a4f0c121676a5f05ecdf7e834005272c4
masterccooddee <arthurhmy@gmail.com>
2023-05-09 02:15:15 +0800

    add string

--- set_time.v
+++ set_time.v
@@ -8,14 +8,21 @@ input button_l;
 input button_r;
 input [3:0]mode;
 
-output [15:0]year;
-output [7:0]month;
-output [7:0]day;
-output [7:0]hour;
-output [7:0]minute;
-output [7:0]sec;
+output reg[15:0]year = 16'h2023;
+output reg[7:0]month = 8'h1;
+output reg[7:0]day = 8'h1;
+output reg[7:0]hour = 0;
+output reg[7:0]minute = 0;
+output reg[7:0]sec = 0;
 
 reg start_set = 0;
+reg [75:0] set_string;
+
+always @(*) begin
+    
+   set_string <= {year + 4'd10 + month + 4'd10 + day + 4'd10 + hour + 4'd10 + minute + 4'd10 + sec};
+ 
+end
 
 always @(mode) begin
     if(mode == 0)
@@ -25,7 +32,9 @@ always @(mode) begin
         start_set <= 0;
 end
 
-
+always @(posedge clk) begin
+    
+end
 
 
 endmodule //set_time

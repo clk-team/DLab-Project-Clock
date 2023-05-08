e36245b7fe0ff47b1744793914f1a55f6ac6cb61
ddww0505 <wangys0505@gmail.com>
2023-05-09 01:17:36 +0800

    ddww

--- main.v
+++ main.v
@@ -11,11 +11,11 @@ module main(
     output sound //聲音
 );
 
-reg hour, minute, second;
+reg [15:0]year, [5:0]month, [10:0]day, [10:0]hour, [10:0]minute, [10:0]second, [10:0]week;  //年、月、日、時、分、秒、星期
 reg newclk;  //除頻
 reg secclk, msecclk;  //秒、毫秒
 divider second(clk, secclk, msecclk, newclk);  //秒、毫秒
-reg num = 0;
+reg num = 0;  //拿來弄電晶體
 
 always @(newclk)
 begin
@@ -24,5 +24,6 @@ begin
     num = 1;
 end
 
-shower(num, show);
+time uuu(secclk, year, month, day, hour, minute, second, week); //基礎時間
+shower ggg(num, show);
 sevenseg kkk(num, seg);

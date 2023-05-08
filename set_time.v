75a77e9ec77e5ad71f731c8e6162fd239aac9010
masterccooddee <arthurhmy@gmail.com>
2023-05-09 01:55:38 +0800

    change button

--- set_time.v
+++ set_time.v
@@ -1,9 +1,12 @@
 `timescale 1ns/1ps
 
-module set_time (clk, button_mid, year, month, day, hour, minute, sec);
+module set_time (clk, button_mid, button_r, button_l, year, month, day, hour, minute, sec, mode);
 
 input clk;
 input button_mid;
+input button_l;
+input button_r;
+input [3:0]mode;
 
 output [15:0]year;
 output [7:0]month;
@@ -12,37 +15,17 @@ output [7:0]hour;
 output [7:0]minute;
 output [7:0]sec;
 
-reg [3:0] count = 0;
-reg [31:0] timer;
-reg [31:0] wait_c = 0;
 reg start_set = 0;
 
-always @(posedge clk) begin
-
-    if(button_mid == 1)
-        timer <= timer + 1;
-        else begin
-            timer <= 0;
-            wait_c <= 0;
-        end
-            
-
-    if(timer >= 2500000)begin
-        
-        wait_c <= wait_c + 1;    
+always @(mode) begin
+    if(mode == 0)
+        start_set <= 1;
     
-    end
+    else
+        start_set <= 0;
 end
 
-always @(posedge clk) begin
-    
-    if(wait_c == 50000000*2)
-        start_set <= ~start_set;
-    
-end
 
-always @(posedge start_set) begin
-    
-end
+
 
 endmodule //set_time

3aed08b46fd02559ad7019b00a570155c08c5dbc
masterccooddee <arthurhmy@gmail.com>
2023-05-09 01:04:27 +0800

    define io

--- set_time.v
+++ set_time.v
@@ -1,7 +1,15 @@
 `timescale 1ns/1ps
 
-module set_time (
-    
-);
+module set_time (clk, button, year, month, day, hour, minute, sec);
+
+input clk;
+input button;
+
+output [15:0]year;
+output [7:0]month;
+output [7:0]day;
+output [7:0]hour;
+output [7:0]minute;
+output [7:0]sec;
 
 endmodule //set_time

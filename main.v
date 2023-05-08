d299519340b9a7f628f1281588afe89c6d36911f
ddww0505 <wangys0505@gmail.com>
2023-05-08 19:32:31 +0800

    test

--- main.v
+++ main.v
@@ -1,13 +1,13 @@
 
 module main(
-    input clk;
-    input up;
-    input down;
-    input left;
-    input right;
-    output seg[7:0];  //七段顯示器
-    output show[7:0];  //七段顯示器的電晶體
-    output sound; //聲音
+    input clk,
+    input up,
+    input down,
+    input left,
+    input right,
+    output seg[7:0],  //七段顯示器
+    output show[7:0],  //七段顯示器的電晶體
+    output sound //聲音
 );
 
 reg secclk, msecclk;  //秒、毫秒

77c906385a3ad0bba1ee6f15de0e8e4817cb4cc3
ddww0505 <wangys0505@gmail.com>
2023-05-08 19:38:39 +0800

    7seg

--- seven_seg.v
+++ seven_seg.v
@@ -7,5 +7,14 @@ module seven_seg{
 always @(num)
   begin 
       case(num)
-        0: seg = 8'00000010;
+        0 : seg = 8'b00000011;
+        1 : seg = 8'b10011111;
+        2 : seg = 8'b00100101;
+        3 : seg = 8'b00001101;
+        4 : seg = 8'b10011001;
+        5 : seg = 8'b01001001;
+        6 : seg = 8'b01000001;
+        7 : seg = 8'b00011111;
+        8 : seg = 8'b00000001;
+        9 : seg = 8'b00001001;
   end

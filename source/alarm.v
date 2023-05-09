f7df869e1a91af46bf216f93acc511b2907ae14b
ddww0505 <wangys0505@gmail.com>
2023-05-09 20:18:06 +0800

    alarm

--- source/alarm.v
+++ source/alarm.v
@@ -1,12 +1,12 @@
 
 module alarm(
-    input clk,
+    input newclk,
     input mode,
     input [10:0]hour,
     input [10:0]minute,
     input [10:0]second, 
     input middle,
-    output[2:0]alarm_mode,
+    output reg[2:0]alarm_mode,
     output reg do
     );
 
@@ -23,15 +23,31 @@ module alarm(
     temp = middle;
     do = 0;
     open_do = 0;  //open_do == 1 代表編譯狀態
+    alarm_mode = 0;
+  end
+always @(posedege newclk)
+begin
+  if(temp == 0 && middle == 1)//轉換狀態
+  begin
+    if(open_do == 0)  //開始編譯
+      alarm_mode = 1;
+    if(open_do == 1)  //結束編譯
+      alarm_mode = 0;
+      #100_000
+     open_do = ~open_do;
+        temp = 1;
   end
 
-  if(temp == 0 && middle == 1)
-     begin
-        open_do = ~open_do;
-        temp = 1;
-     end
   if(middle == 0 && temp == 1)
   begin
-    temp = 0;
+        temp = 0;
+  end
+   
+  if(alarm_mode == 1 &&open_do == 0)
+    begin
+        temp_hour = hour;
+        temp_minute = minute;
+        temp_second = second;
+    end
   end
 endmodule

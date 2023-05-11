bf85839421c52d610198f5c5529f4fe74d1cecd2
masterccooddee <arthurhmy@gmail.com>
2023-05-12 02:33:35 +0800

    current state: finish add and subtract of the time

--- set_time.v
+++ set_time.v
@@ -22,6 +22,7 @@ reg start_set = 0;
 reg [83:0] set_string;
 reg[4:0] count = 20;
 reg[31:0] timer;
+reg[31:0] timer2;
 
 reg [3:0]year_th = 2;
 reg [3:0]year_h = 0;
@@ -45,10 +46,10 @@ reg [3:0]sec_u = 0;
 
 reg [14:0]year_d;
 reg [3:0]month_d;
-reg [3:0]day_d;
-reg [3:0]hour_d;
-reg [3:0]min_d;
-reg [3:0]sec_d;
+reg [4:0]day_d;
+reg [5:0]hour_d;
+reg [5:0]min_d;
+reg [5:0]sec_d;
 
 reg [14:0]year_f;
 reg [3:0]month_f;
@@ -115,22 +116,22 @@ always @(posedge clk) begin
 
             if(count >= 20)
                 count <= 0;
-            else if(count == 3)
-                count <= 5;
-            else if(count == 6 )
-                count <= 8;
+            else if(count == 1)
+                count <= 3;
+            else if(count == 4 )
+                count <= 6;
+            else if(count == 7)
+                count <= 9;
             else if(count == 9)
                 count <= 11;
-            else if(count == 11)
-                count <= 13;
-            else if(count == 14)
-                count <= 16;
-            else if(count == 17)
-                count <= 19;
+            else if(count == 12)
+                count <= 14;
+            else if(count == 15)
+                count <= 17;
             else
                 count <= count + 1;
 
-            // if(count == 4 || count == 7 || count == 10 || count == 12 || count == 15 || count == 18) //4 7 10 12 15 18
+            // if(count == 4 || count == 7 || count == 10 || count == 12 || count == 15 || count == 18) //2 5 8 10 13 16
             //     count <= count + 1;
 
         end
@@ -164,18 +165,18 @@ end
 always @(posedge clk or posedge start_set) begin
 
     if(button_up == 1 || button_down == 1)
-        timer <= timer + 1;
+        timer2 <= timer2 + 1;
         else
-            timer <= 0;
+            timer2 <= 0;
 
-    if(timer == 2500000)begin
-        if(button_up == 1) begin //4 7 10 12 15 18
+    if(timer2 == 3)begin //2500000
+        if(button_up == 1) begin //2 5 8 10 13 16
 
             case (count)
             /*year*/
             20: 
                 begin
-                    if(year_th > 9)
+                    if(year_th >= 9)
                         year_th <= 0;
                     else
                         year_th <= year_th + 1; 
@@ -183,91 +184,91 @@ always @(posedge clk or posedge start_set) begin
 
             19:
                 begin
-                    if(year_h > 9)
+                    if(year_h >= 9)
                         year_h <= 0;
                     else
                         year_h <= year_h + 1; 
                 end
 
-            17:
+            18:
                 begin
-                    if(year_t > 9)
+                    if(year_t >= 9)
                         year_t <= 0;
                     else
                         year_t <= year_t + 1; 
                 end
 
-            16:
+            17:
                 begin
-                    if(year_u > 9)
+                    if(year_u >= 9)
                         year_u <= 0;
                     else
                         year_u <= year_u + 1; 
                 end
 
             /*month*/
-            14:
+            15:
                 begin
-                    if(month_t > 1)
+                    if(month_t >= 1)
                         month_t <= 0;
                     else
                         month_t <= month_t + 1; 
                 end
 
-            13:
+            14:
                 begin
-                    if(month_u > 9)
+                    if(month_u >= 9)
                         month_u <= 0;
                     else
                         month_u <= month_u + 1; 
                 end
 
             /*day*/
-            11:
+            12:
                 begin
-                    if(day_t > 3)
+                    if(day_t >= 3)
                         day_t <= 0;
                     else
                         day_t <=day_t + 1; 
                 end
 
-            9:
+            11:
                 begin
-                    if(day_u > 9)
+                    if(day_u >= 9)
                         day_u <= 0;
                     else
                         day_u <=day_u + 1; 
                 end
 
             /*hour*/
-            6:
+            7:
                 begin
-                    if(hour_t > 2)
+                    if(hour_t >= 2)
                         hour_t <= 0;
                     else
                         hour_t <=hour_t + 1; 
                 end
 
-            5:
+            6:
                 begin
-                    if(hour_u > 9)
+                    if(hour_u >= 9)
                         hour_u <= 0;
                     else
                         hour_u <=hour_u + 1; 
                 end
 
             /*minute*/
-            3:
+            4:
                 begin
-                    if(min_t > 5)
+                    if(min_t >= 5)
                         min_t <= 0;
                     else
                         min_t <=min_t + 1; 
                 end
 
-            2:
+            3:
                 begin
-                    if(min_u > 9)
+                    if(min_u >= 9)
                         min_u <= 0;
                     else
                         min_u <=min_u + 1; 
@@ -276,14 +277,14 @@ always @(posedge clk or posedge start_set) begin
             /*sec*/
             1:
                 begin
-                    if(sec_t > 5)
+                    if(sec_t >= 5)
                         sec_t <= 0;
                     else
                         sec_t <=sec_t + 1; 
                 end
             0:
                 begin
-                    if(sec_u > 9)
+                    if(sec_u >= 9)
                         sec_u <= 0;
                     else
                         sec_u <= sec_u + 1; 
@@ -300,7 +301,7 @@ always @(posedge clk or posedge start_set) begin
             /*year*/
             20: 
                 begin
-                    if(year_th < 0)
+                    if(year_th <= 0)
                         year_th <= 9;
                     else
                         year_th <= year_th - 1; 
@@ -308,91 +309,91 @@ always @(posedge clk or posedge start_set) begin
 
             19:
                 begin
-                    if(year_h < 0)
+                    if(year_h <= 0)
                         year_h <= 9;
                     else
                         year_h <= year_h - 1; 
                 end
 
-            17:
+            18:
                 begin
-                    if(year_t < 0)
+                    if(year_t <= 0)
                         year_t <= 9;
                     else
                         year_t <= year_t - 1; 
                 end
 
-            16:
+            17:
                 begin
-                    if(year_u < 0)
+                    if(year_u <= 0)
                         year_u <= 9;
                     else
                         year_u <= year_u - 1; 
                 end
 
             /*month*/
-            14:
+            15:
                 begin
-                    if(month_t < 0)
+                    if(month_t <= 0)
                         month_t <= 1;
                     else
                         month_t <= month_t - 1; 
                 end
 
-            13:
+            14:
                 begin
-                    if(month_u < 0)
+                    if(month_u <= 0)
                         month_u <= 9;
                     else
                         month_u <= month_u - 1; 
                 end
 
             /*day*/
-            11:
+            12:
                 begin
-                    if(day_t < 0)
+                    if(day_t <= 0)
                         day_t <= 3;
                     else
                         day_t <=day_t - 1; 
                 end
 
-            9:
+            11:
                 begin
-                    if(day_u < 0)
+                    if(day_u <= 0)
                         day_u <= 9;
                     else
                         day_u <=day_u - 1; 
                 end
 
             /*hour*/
-            6:
+            7:
                 begin
-                    if(hour_t < 0)
+                    if(hour_t <= 0)
                         hour_t <= 5;
                     else
                         hour_t <=hour_t - 1; 
                 end
 
-            5:
+            6:
                 begin
-                    if(hour_u < 0)
+                    if(hour_u <= 0)
                         hour_u <= 9;
                     else
                         hour_u <=hour_u - 1; 
                 end
 
             /*minute*/
-            3:
+            4:
                 begin
-                    if(min_t < 0)
+                    if(min_t <= 0)
                         min_t <= 5;
                     else
                         min_t <=min_t - 1; 
                 end
 
-            2:
+            3:
                 begin
-                    if(min_u < 0)
+                    if(min_u <= 0)
                         min_u <= 9;
                     else
                         min_u <=min_u - 1; 
@@ -401,14 +402,14 @@ always @(posedge clk or posedge start_set) begin
             /*sec*/
             1:
                 begin
-                    if(sec_t < 0)
+                    if(sec_t <= 0)
                         sec_t <= 5;
                     else
                         sec_t <=sec_t - 1; 
                 end
             0:
                 begin
-                    if(sec_u < 0)
+                    if(sec_u <= 0)
                         sec_u <= 9;
                     else
                         sec_u <= sec_u - 1; 

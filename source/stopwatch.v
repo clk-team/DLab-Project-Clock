7ef227798cd6435821482da66cc11f8857bb0207
masterccooddee <arthurhmy@gmail.com>
2023-05-15 01:55:15 +0800

    123

--- source/stopwatch.v
+++ source/stopwatch.v
@@ -9,60 +9,50 @@ module stopwatch(clk,start,reset,quick,min_10,min_1,sec_10,sec_1,milli_10,milli_
     reg [5:0]count=0;
     reg min_clk=0;
     reg debounce=0;
-    reg go=0;
+    reg go=1;
     reg millic=0,sc=0,mc=0;
     reg [5:0] sec=0;
     reg [5:0] min=0;
-    reg [5:0] milli=0;
+    reg [6:0] milli=0;
     reg milli_clk=0;
     integer i;
+    reg [31:0]time_counter=0;
     
-    always@(posedge clk) begin
-        if(reset) begin
-            min   <= 0;
-            sec   <= 0;
-            milli <= 0;
-            go    <= 0;
-        end
-    end
+//    always@(posedge clk) begin
+//        if(reset) begin
+//            min   <= 0;
+//            sec   <= 0;
+//            milli <= 0;
+//            go    <= 0;
+//        end
+//    end
     
      always@(posedge clk) begin
-        if(go==0)    begin
-            counter <= counter;
+        if(time_counter >= 2500000)    begin
+            if(counter == 499999) begin //slow counting
+                counter <= 0;
+                milli_clk <= ~ milli_clk;
+            end
+            else
+                counter <= counter + 1'b1;
+         
         end
-        else if(go==1)   begin
-            if(quick==0)  begin
-                if(counter == 49999) begin //slow counting
-                    counter <= 0;
-                    milli_clk <= ~ milli_clk;
-                end
-                else
-                    counter <= counter + 1'b1;
-            end
-            else begin
-                if(counter == 1) begin //fast counting
-                    counter <= 0;
-                    milli_clk <= ~milli_clk;
-                end
-                else
-                    counter <= counter + 1'b1;
-            end
+        else begin
+            counter  <= counter;
         end
     end      
         
     
     always@(posedge clk) begin
-        if(start && !debounce) begin
-            debounce <= 1;
-            go <= ~go;
+        if(start) begin
+            time_counter <= time_counter + 1'b1;
         end
-        else if(!start && debounce) begin
-            debounce <= 0;
-        end
+        else
+            time_counter <= 0;
     end
     
     always@(posedge milli_clk) begin
-        if(milli > 59) begin
+        if(milli > 99) begin
             milli <= 0;
             millic <= 1;
         end
@@ -91,7 +81,7 @@ module stopwatch(clk,start,reset,quick,min_10,min_1,sec_10,sec_1,milli_10,milli_
         milli_1  = 4'd0;
         milli_10 = 4'd0;
     
-    for(i = 5 ; i >= 0 ; i = i - 1) begin
+    for(i = 6 ; i >= 0 ; i = i - 1) begin
     
         if(milli_1 >= 5)
            milli_1 = milli_1 + 3;
